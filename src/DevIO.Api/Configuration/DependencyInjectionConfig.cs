4a026315a10f308317114537893c6998fcfe0a3a
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-13 18:28:44 -0300

    Adicionando injeção de dependencias que estavam faltando

--- src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 
@@ -9,8 +11,14 @@ public static class DependencyInjectionConfig
     public static IServiceCollection ResolveDependencies(this IServiceCollection services)
     {
         services.AddScoped<MeuDbContext>();
-        services.AddTransient<IFornecedorRepository, FornecedorRepository>();
-        
+        services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+        services.AddScoped<IFornecedorService, FornecedorService>();
+        services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+        services.AddScoped<INotificador, Notificador>();
+        services.AddScoped<IProdutoService, ProdutoService>();
+        services.AddScoped<IProdutoRepository, ProdutoRepository>(); 
+
         return services;
     }
 }
