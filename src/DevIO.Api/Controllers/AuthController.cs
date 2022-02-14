95f896b9d9b232e4c9b70530c4e3305c654639dd
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-14 19:18:25 -0300

    Atualização para que na hora de logar e se cadastrar o método de gerar o token retorne mais informações como as claims daquele usuario. Não é necessario fazer se quiser, é apenas uma informação extra apra o front-end

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -88,7 +88,7 @@ public class AuthController : MainController
         return CustomReponse(loginUser);
     }
 
-    private async Task<string> GerarJwt(string email)
+    private async Task<LoginResponseViewModel> GerarJwt(string email)
     {
         var user = await _userManager.FindByEmailAsync(email);
         var claims = await _userManager.GetClaimsAsync(user);
@@ -121,7 +121,19 @@ public class AuthController : MainController
 
         var encodedToken = tokenHandler.WriteToken(token);
 
-        return encodedToken;
+        var response = new LoginResponseViewModel
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+            UserToken = new UserTokenViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+            }
+        };
+
+        return response;
     }
 
     private static long ToUnixEpochDate(DateTime date)
