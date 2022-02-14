b79bb02f98f5bda898be2a88ff21fdaaebe1f98d
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-14 17:51:40 -0300

    Criando as configurações iniciais de um JWT e criando o JTW basico para a API

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
-﻿using DevIO.Api.ViewModels;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+using DevIO.Api.Extensions;
+using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DevIO.Api.Controllers;
 
@@ -11,13 +17,16 @@ public class AuthController : MainController
 {
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppSettings _appSettings;
 
     public AuthController(INotificador notificador, 
                           SignInManager<IdentityUser> signInManager, 
-                          UserManager<IdentityUser> userManager) : base(notificador)
+                          UserManager<IdentityUser> userManager,
+                          IOptions<AppSettings> appSettings) : base(notificador)
     {
         _signInManager = signInManager;
         _userManager = userManager;
+        _appSettings = appSettings.Value;
     }
 
     [HttpPost("nova-conta")]
@@ -40,7 +49,7 @@ public class AuthController : MainController
         if (result.Succeeded)
         {
             await _signInManager.SignInAsync(user, false);
-            return CustomReponse(registerUser);
+            return CustomReponse(GerarJwt());
         }
 
         foreach (var error in result.Errors)
@@ -63,7 +72,7 @@ public class AuthController : MainController
 
         if (result.Succeeded)
         {
-            return CustomReponse(loginUser);
+            return CustomReponse(GerarJwt());
         }
         
         if (result.IsLockedOut)
@@ -74,5 +83,22 @@ public class AuthController : MainController
         
         NotificarErro("Usuário ou senha incorretos");
         return CustomReponse(loginUser);
+    }
+
+    private string GerarJwt()
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = _appSettings.Emissor,
+            Audience = _appSettings.ValidoEm,
+            Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.Sha256)
+        });
+
+        var encodedToken = tokenHandler.WriteToken(token);
+
+        return encodedToken;
     }
 }
