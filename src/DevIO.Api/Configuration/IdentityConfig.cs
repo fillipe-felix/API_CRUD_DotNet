19d7c870902822bccf5c8e661c6270023c8d366b
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-14 16:41:32 -0300

    Criando as configurações iniciais para realizar authenticação

---
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using DevIO.Api.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Api.Configuration;
+
+public static class IdentityConfig
+{
+    public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+                                                              IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddIdentityCore<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+        
+        return services;
+    }
+}
